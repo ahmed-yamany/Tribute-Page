6f7e5f7b4aea53f02b51884e4f76ab65c7f647b5
ahmedyamany <yamany28@students.mans.edu.eg>
2020-02-15 19:52:48 +0200

    story of linux

--- scripts.js
+++ scripts.js
@@ -7,3 +7,21 @@ selectElement('.open').addEventListener('click', () => {
 selectElement('.close').addEventListener('click', () => {
     selectElement('.nav-list').classList.remove('active');
 });
+selectElement('.current').addEventListener('click', () => {
+    selectElement('.nav-list').classList.remove('active');
+});
+selectElement('.current1').addEventListener('click', () => {
+    selectElement('.nav-list').classList.remove('active');
+});
+selectElement('.current2').addEventListener('click', () => {
+    selectElement('.nav-list').classList.remove('active');
+});
+selectElement('.current3').addEventListener('click', () => {
+    selectElement('.nav-list').classList.remove('active');
+});
+selectElement('.current4').addEventListener('click', () => {
+    selectElement('.nav-list').classList.remove('active');
+});
+selectElement('.current5').addEventListener('click', () => {
+    selectElement('.nav-list').classList.remove('active');
+});
